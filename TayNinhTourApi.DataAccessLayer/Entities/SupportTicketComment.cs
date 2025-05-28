a542bc3a14488bb1f8fa2f420aca469586646189
Bùi Minh Quang <155402775+kiettase171131@users.noreply.github.com>
2025-05-28 22:17:40 +0700

    fix ticket

---
+++ TayNinhTourApi.DataAccessLayer/Entities/SupportTicketComment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TayNinhTourApi.DataAccessLayer.Entities
+{
+    public class SupportTicketComment : BaseEntity
+    {
+        
+        public Guid SupportTicketId { get; set; }
+        public virtual SupportTicket SupportTicket { get; set; } = null!;
+        public Guid CreatedById { get; set; }
+        public virtual User CreatedBy { get; set; } = null!;
+        public string CommentText { get; set; } = null!;
+    }
+}
