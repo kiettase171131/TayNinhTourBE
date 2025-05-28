8591739542c8dcae0c2780bbedf16ae7289baa4d
Bùi Minh Quang <144604032+LebronBui@users.noreply.github.com>
2025-05-28 22:10:49 +0700

    role

---
+++ TayNinhTourApi.BusinessLogicLayer/Common/Constants.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TayNinhTourApi.BusinessLogicLayer.Common
+{
+    public static class Constants
+    {
+        public const int TokenExpiredTime = 1; // in day
+
+        public const string RoleAdminName = "Admin";
+        public const string RoleTourCompanyName = "Tour Company";
+        public const string RoleUserName = "User";
+        public const string RoleTourGuideName = "Tour Guide";
+
+        public const int MaxFailedAttempts = 5;
+
+        public const int PageSizeDefault = 10;
+
+        public const int PageIndexDefault = 1;
+    }
+}
