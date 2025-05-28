92445934f3fb9cf226dda65f4e91759645c76cff
Bùi Minh Quang <155402775+kiettase171131@users.noreply.github.com>
2025-05-28 22:16:04 +0700

    update ctrler

---
+++ TayNinhTourApi.Controller/Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlX.XDevAPI.Common;
+using TayNinhTourApi.BusinessLogicLayer.Common;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.AccountDTO;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.ApplicationDTO;
+using TayNinhTourApi.BusinessLogicLayer.Services.Interface;
+using TayNinhTourApi.Controller.Helper;
+
+namespace TayNinhTourApi.Controller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        private readonly ITourGuideApplicationService _tourGuideApplicationService;
+
+        public AccountController(IAccountService accountService, ITourGuideApplicationService tourGuideApplicationService )
+        {
+            _accountService = accountService;
+            _tourGuideApplicationService = tourGuideApplicationService;
+        }
+        
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPut("change-password")]
+        public async Task<IActionResult> ChangePassword(PasswordDTO password)
+        {      
+                CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+                var result = await _accountService.ChangePassword(password, currentUserObject);
+                return StatusCode(result.StatusCode, result);
+
+        }
+        
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPut("edit-profile")]
+        public async Task<IActionResult> UpdateProfile(EditAccountProfileDTO editAccountProfileDTO)
+        {
+                CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+                var result = await _accountService.UpdateProfile(editAccountProfileDTO, currentUserObject);
+                 return StatusCode(result.StatusCode, result);
+
+        }
+        
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("profile")]
+        public async Task<IActionResult> GetProfile()
+        {
+                CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+                var result = await _accountService.GetProfile(currentUserObject);
+                return StatusCode(result.StatusCode, result);
+
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles ="User")]
+        [HttpPost("tourguide-application")]
+       
+        public async Task<IActionResult> Submit([FromForm] SubmitApplicationDto submitApplicationDto)
+        {
+            CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+            var result = await _tourGuideApplicationService.SubmitAsync(submitApplicationDto,currentUserObject);
+            return StatusCode(result.StatusCode, result);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("View-tourguideapplication")]
+        public async Task<IActionResult> ListMyApplications()
+        {
+            CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+            var list = await _tourGuideApplicationService.ListByUserAsync(currentUserObject.Id);
+            return Ok(list);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPut("edit-Avatar")]
+        
+        public async Task<IActionResult> UpdateAvatar([FromForm] AvatarDTO avatarDTO)
+        {
+            CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+            var result = await _accountService.UpdateAvatar(avatarDTO, currentUserObject);
+            return StatusCode(result.StatusCode, result);
+
+        }
+    }
+}
---
+++ TayNinhTourApi.Controller/Controllers/SupportTicketsController.cs
@@ -0,0 +1,86 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.AccountDTO;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.SupTicketDTO;
+using TayNinhTourApi.BusinessLogicLayer.Services;
+using TayNinhTourApi.BusinessLogicLayer.Services.Interface;
+using TayNinhTourApi.Controller.Helper;
+
+namespace TayNinhTourApi.Controller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupportTicketsController : ControllerBase
+    {
+        private readonly ISupportTicketService _service;
+        public SupportTicketsController(ISupportTicketService service)
+        {
+            _service = service;
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme,Roles ="User")]    
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] RequestCreateTicketDto dto)
+        {
+            CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+            var response = await _service.CreateTicketAsync(dto , currentUserObject);
+            return StatusCode(response.StatusCode, response);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme,Roles = "User")]
+        // GET: api/SupportTickets
+        [HttpGet("User")]
+        public async Task<IActionResult> ListForUser()
+        {
+            CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+            var tickets = await _service.GetTicketsForUserAsync(currentUserObject.Id);
+            return Ok(tickets);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+        // GET: api/SupportTickets/Admin
+        [HttpGet("Admin")]
+        public async Task<IActionResult> ListForAdmin()
+        {
+            CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+            var tickets = await _service.GetTicketsForAdminAsync(currentUserObject.Id);
+            return Ok(tickets);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        // GET: api/SupportTickets/{id}
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var ticket = await _service.GetTicketDetailsAsync(id);
+            if (ticket == null) return NotFound();
+            return Ok(ticket);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        // POST: api/SupportTickets/{id}/comments
+        [HttpPost("{id:guid}/comments")]
+        public async Task<IActionResult> Reply(Guid id, [FromBody] CreateCommentDto dto)
+        {
+            CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+           var response = await _service.ReplyAsync(id, currentUserObject.Id, dto.CommentText);
+            return StatusCode(response.StatusCode, response);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        // PUT: api/SupportTickets/{id}/status
+        [HttpPut("{id:guid}/status")]
+        public async Task<IActionResult> ChangeStatus(Guid id, [FromBody] ChangeStatusDto dto)
+        {
+           var response =  await _service.ChangeStatusAsync(id, dto.NewStatus);
+            return StatusCode(response.StatusCode, response);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        // DELETE: api/SupportTickets/{id}
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            CurrentUserObject currentUserObject = await TokenHelper.Instance.GetThisUserInfo(HttpContext);
+            var response = await _service.DeleteTicketAsync(id, currentUserObject.Id);
+            return StatusCode(response.StatusCode, response);
+        }
+    }
+}
