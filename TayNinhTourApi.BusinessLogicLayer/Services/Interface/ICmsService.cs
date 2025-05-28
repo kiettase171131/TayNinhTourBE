3f90f41489ed99c3c4a338807c1da895dd96dc66
Bùi Minh Quang <155402775+kiettase171131@users.noreply.github.com>
2025-05-28 22:14:57 +0700

    update base

---
+++ TayNinhTourApi.BusinessLogicLayer/Services/Interface/ICmsService.cs
@@ -0,0 +1,19 @@
+﻿using TayNinhTourApi.BusinessLogicLayer.DTOs;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Request.Cms;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Request.TourCompany;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Response.Cms;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Response.TourCompany;
+
+namespace TayNinhTourApi.BusinessLogicLayer.Services.Interface
+{
+    public interface ICmsService
+    {
+        Task<BaseResposeDto> DeleteUserAsync(Guid id);
+        Task<ResponseGetTourDto> GetTourByIdAsync(Guid id);
+        Task<ResponseGetToursDto> GetToursAsync(int? pageIndex, int? pageSize, string? textSearch, bool? status);
+        Task<ResponseGetUsersCmsDto> GetUserAsync(int? pageIndex, int? pageSize, string? textSearch, bool? status);
+        Task<ResponseGetUserByIdCmsDto> GetUserByIdAsync(Guid id);
+        Task<BaseResposeDto> UpdateTourAsync(RequestUpdateTourCmsDto request, Guid id, Guid updatedBy);
+        Task<BaseResposeDto> UpdateUserAsync(RequestUpdateUserCmsDto request, Guid id);
+    }
+}
