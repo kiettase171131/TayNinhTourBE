efcb07f771b4292b57eb5d48ae9446e1f0747627
Bùi Minh Quang <155402775+kiettase171131@users.noreply.github.com>
2025-05-28 22:14:47 +0700

    upload avt

---
+++ TayNinhTourApi.BusinessLogicLayer/Services/CmsService.cs
@@ -0,0 +1,260 @@
+﻿using AutoMapper;
+using LinqKit;
+using TayNinhTourApi.BusinessLogicLayer.Common;
+using TayNinhTourApi.BusinessLogicLayer.DTOs;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Request.Cms;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Request.TourCompany;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Response.Cms;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Response.TourCompany;
+using TayNinhTourApi.BusinessLogicLayer.Services.Interface;
+using TayNinhTourApi.DataAccessLayer.Entities;
+using TayNinhTourApi.DataAccessLayer.Repositories.Interface;
+using TayNinhTourApi.DataAccessLayer.UnitOfWork.Interface;
+
+namespace TayNinhTourApi.BusinessLogicLayer.Services
+{
+    public class CmsService : ICmsService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CmsService(IUserRepository userRepository, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseResposeDto> DeleteUserAsync(Guid id)
+        {
+            // Find user by id
+            var user = await _userRepository.GetByIdAsync(id);
+
+            if (user == null || user.IsDeleted)
+            {
+                return new BaseResposeDto
+                {
+                    StatusCode = 404,
+                    Message = "User not found"
+                };
+            }
+
+            // Delete user
+            user.IsDeleted = true;
+
+            // Save changes to database
+            await _userRepository.SaveChangesAsync();
+
+            return new BaseResposeDto
+            {
+                StatusCode = 200,
+                Message = "User deleted successfully"
+            };
+        }
+
+        public async Task<ResponseGetTourDto> GetTourByIdAsync(Guid id)
+        {
+            var include = new string[] { "CreatedBy", "UpdatedBy", nameof(Tour.Images) };
+
+            var predicate = PredicateBuilder.New<Tour>(x => !x.IsDeleted);
+
+            // Find the branch by id
+            var tour = await _unitOfWork.TourRepository.GetByIdAsync(id, include);
+
+            if (tour == null || tour.IsDeleted)
+            {
+                return new ResponseGetTourDto
+                {
+                    StatusCode = 404,
+                    Message = "Không tìm thấy chi nhánh này",
+                };
+            }
+
+            return new ResponseGetTourDto
+            {
+                StatusCode = 200,
+                Data = _mapper.Map<TourDto>(tour)
+            };
+        }
+
+        public async Task<ResponseGetToursDto> GetToursAsync(int? pageIndex, int? pageSize, string? textSearch, bool? status)
+        {
+            var include = new string[] { "CreatedBy", "UpdatedBy", nameof(Tour.Images) };
+
+            // Default values for pagination
+            var pageIndexValue = pageIndex ?? Constants.PageIndexDefault;
+            var pageSizeValue = pageSize ?? Constants.PageSizeDefault;
+
+            // Create a predicate for filtering
+            var predicate = PredicateBuilder.New<Tour>(x => !x.IsDeleted);
+
+            // Check if textSearch is null or empty
+            if (!string.IsNullOrEmpty(textSearch))
+            {
+                predicate = predicate.And(x => (x.Title != null && x.Title.Contains(textSearch, StringComparison.OrdinalIgnoreCase)));
+            }
+
+            // Check if status is null or empty
+            if (status.HasValue)
+            {
+                predicate = predicate.And(x => x.IsActive == status);
+            }
+
+            // Get tours from repository
+            var tours = await _unitOfWork.TourRepository.GenericGetPaginationAsync(pageIndexValue, pageSizeValue, predicate, include);
+
+            var totalTours = tours.Count();
+            var totalPages = (int)Math.Ceiling((double)totalTours / pageSizeValue);
+
+            return new ResponseGetToursDto
+            {
+                StatusCode = 200,
+                Data = _mapper.Map<List<TourDto>>(tours),
+                TotalRecord = totalTours,
+                TotalPages = totalPages,
+            };
+        }
+
+        public async Task<ResponseGetUsersCmsDto> GetUserAsync(int? pageIndex, int? pageSize, string? textSearch, bool? status)
+        {
+            // Set page index and page size
+            var pageIndexValue = pageIndex ?? Constants.PageIndexDefault;
+            var pageSizeValue = pageSize ?? Constants.PageSizeDefault;
+
+            // Create predicate for filtering
+            var predicate = PredicateBuilder.New<User>(x => !x.IsDeleted);
+
+            if (!string.IsNullOrEmpty(textSearch))
+            {
+                predicate = predicate.And(x => x.Name.Contains(textSearch, StringComparison.OrdinalIgnoreCase) || x.Email.Contains(textSearch, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (status.HasValue)
+            {
+                predicate = predicate.And(x => x.IsActive == status.Value);
+            }
+
+            // Get users from repository
+            var users = await _userRepository.GenericGetPaginationAsync(pageIndexValue, pageSizeValue, predicate);
+
+            var totalUsers = users.Count();
+            var totalPages = (int)Math.Ceiling((double)totalUsers / pageSizeValue);
+
+            return new ResponseGetUsersCmsDto
+            {
+                StatusCode = 200,
+                Data = _mapper.Map<List<UserCmsDto>>(users),
+                TotalRecord = totalUsers,
+                TotalPages = totalPages,
+            };
+        }
+
+        public async Task<ResponseGetUserByIdCmsDto> GetUserByIdAsync(Guid id)
+        {
+            // Find user by id
+            var user = await _userRepository.GetByIdAsync(id);
+
+            if (user == null || user.IsDeleted)
+            {
+                return new ResponseGetUserByIdCmsDto
+                {
+                    StatusCode = 404,
+                    Message = "User not found"
+                };
+            }
+
+            return new ResponseGetUserByIdCmsDto
+            {
+                StatusCode = 200,
+                Data = _mapper.Map<UserCmsDto>(user)
+            };
+        }
+
+        public async Task<BaseResposeDto> UpdateTourAsync(RequestUpdateTourCmsDto request, Guid id, Guid updatedById)
+        {
+
+            // Find tour by id
+            var existingTour = await _unitOfWork.TourRepository.GetByIdAsync(id);
+
+            if (existingTour == null || existingTour.IsDeleted)
+            {
+                return new BaseResposeDto
+                {
+                    StatusCode = 404,
+                    Message = "Tour not found"
+                };
+            }
+
+            // Check if the tour is already approved
+            if (existingTour.IsApproved)
+            {
+                return new BaseResposeDto
+                {
+                    StatusCode = 400,
+                    Message = "Tour đã được phê duyệt"
+                };
+            }
+
+            // Update tour
+            existingTour.Status = request.Status ?? existingTour.Status;
+            existingTour.CommentApproved = request.CommentApproved ?? existingTour.CommentApproved;
+
+            existingTour.IsApproved = true;
+            existingTour.UpdatedById = updatedById;
+
+            // Get user by id
+            var user = await _userRepository.GetByIdAsync(updatedById);
+            if (user == null || user.IsDeleted)
+            {
+                return new BaseResposeDto
+                {
+                    StatusCode = 404,
+                    Message = "User not found"
+                };
+            }
+
+            // Set the updated by user
+            existingTour.UpdatedBy = user;
+
+            // Save changes to database
+            await _unitOfWork.SaveChangesAsync();
+
+            return new BaseResposeDto
+            {
+                StatusCode = 200,
+                Message = "Cập nhật Tour thành công"
+            };
+        }
+
+        public async Task<BaseResposeDto> UpdateUserAsync(RequestUpdateUserCmsDto request, Guid id)
+        {
+            // Find user by email
+            var existingUser = await _userRepository.GetByIdAsync(id);
+
+            if (existingUser == null)
+            {
+                return new BaseResposeDto
+                {
+                    StatusCode = 404,
+                    Message = "User not found"
+                };
+            }
+
+            // Update user
+            existingUser.Name = request.Name ?? existingUser.Name;
+            existingUser.PhoneNumber = request.PhoneNumber ?? existingUser.PhoneNumber;
+            existingUser.Avatar = request.Avatar ?? existingUser.Avatar;
+            existingUser.IsActive = request.IsActive ?? existingUser.IsActive;
+
+            // Save changes to database
+            await _userRepository.SaveChangesAsync();
+
+            return new BaseResposeDto
+            {
+                StatusCode = 200,
+                Message = "User updated successfully"
+            };
+        }
+    }
+}
