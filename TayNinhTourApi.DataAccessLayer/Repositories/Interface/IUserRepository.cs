39d00f3c63ea1cb6f1c4d6cd6e3b79eb4c1e9f45
Bùi Minh Quang <144604032+LebronBui@users.noreply.github.com>
2025-05-28 22:18:32 +0700

    uipdate2

---
+++ TayNinhTourApi.DataAccessLayer/Repositories/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using TayNinhTourApi.DataAccessLayer.Entities;
+
+namespace TayNinhTourApi.DataAccessLayer.Repositories.Interface
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User?> FindUserByRefreshToken(Guid userId, string refreshToken);
+        Task<User?> GetUserByEmailAsync(string email, string[]? includes = null);
+        Task<bool> CheckEmailExistAsync(string email);
+        Task<IEnumerable<User>> ListAdminsAsync();
+
+    }
+}
