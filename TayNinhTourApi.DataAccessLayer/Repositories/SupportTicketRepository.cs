9db110590ae80c79c72761334051755449799d66
Bùi Minh Quang <144604032+LebronBui@users.noreply.github.com>
2025-05-28 22:09:21 +0700

    update ticket

---
+++ TayNinhTourApi.DataAccessLayer/Repositories/SupportTicketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TayNinhTourApi.DataAccessLayer.Contexts;
+using TayNinhTourApi.DataAccessLayer.Entities;
+using TayNinhTourApi.DataAccessLayer.Repositories.Interface;
+
+namespace TayNinhTourApi.DataAccessLayer.Repositories
+{
+    public class SupportTicketRepository : GenericRepository<SupportTicket>, ISupportTicketRepository
+    {
+        public SupportTicketRepository(TayNinhTouApiDbContext context) : base(context)
+        {
+        }
+
+        public async Task<SupportTicket?> GetWithCommentsAsync(Guid ticketId)
+        {
+           return await _context.SupportTickets
+                  .Include(t => t.Comments)
+                    .ThenInclude(c => c.CreatedBy)
+                  .SingleOrDefaultAsync(t => t.Id == ticketId);
+        }
+
+        public Task<IEnumerable<SupportTicket>> ListByAdminAsync(Guid adminId)
+        {
+           return Task.FromResult(_context.SupportTickets.Where(t => t.AdminId == adminId).AsEnumerable());
+        }
+
+        public Task<IEnumerable<SupportTicket>> ListByUserAsync(Guid userId)
+        {
+            return Task.FromResult(_context.SupportTickets.Where(t => t.UserId == userId).AsEnumerable());
+        }
+    }
+}
---
+++ TayNinhTourApi.DataAccessLayer/Repositories/TourGuideApplicationRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TayNinhTourApi.DataAccessLayer.Contexts;
+using TayNinhTourApi.DataAccessLayer.Entities;
+using TayNinhTourApi.DataAccessLayer.Repositories.Interface;
+
+namespace TayNinhTourApi.DataAccessLayer.Repositories
+{
+    public class TourGuideApplicationRepository : GenericRepository<TourGuideApplication>, ITourGuideApplicationRepository
+    {
+        public TourGuideApplicationRepository(TayNinhTouApiDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<TourGuideApplication>> ListByStatusAsync(ApplicationStatus status)
+        {
+            return await _context.Set<TourGuideApplication>()
+                         .Include(a => a.User)
+                         .Where(a => a.Status == status)
+                         .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TourGuideApplication>> ListByUserAsync(Guid userId)
+        {
+            return await _context.Set<TourGuideApplication>()
+                         .Include(a => a.User)
+                         .Where(a => a.UserId == userId)
+                         .OrderByDescending(a => a.CreatedAt)
+                         .ToListAsync();
+        }
+    }
+}
