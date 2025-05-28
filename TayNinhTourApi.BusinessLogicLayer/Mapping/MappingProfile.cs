b5d9326c130a656aff2534512e23af9e3d730bfb
Bùi Minh Quang <144604032+LebronBui@users.noreply.github.com>
2025-05-28 22:13:50 +0700

    fix

---
+++ TayNinhTourApi.BusinessLogicLayer/Mapping/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Request.Authentication;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Request.TourCompany;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Response.Cms;
+using TayNinhTourApi.BusinessLogicLayer.DTOs.Response.TourCompany;
+using TayNinhTourApi.DataAccessLayer.Entities;
+
+namespace TayNinhTourApi.BusinessLogicLayer.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region User Mapping
+            CreateMap<RequestRegisterDto, User>();
+            CreateMap<User, UserCmsDto>();
+            #endregion
+
+            #region Tour Mapping
+            CreateMap<RequestCreateTourCmsDto, Tour>().ForMember(dest => dest.Images, otp => otp.Ignore());
+            CreateMap<Tour, TourDto>().ForMember(dest => dest.Images, otp => otp.MapFrom(src => src.Images.Select(x => x.Url).ToList()));
+            #endregion
+        }
+    }
+}
