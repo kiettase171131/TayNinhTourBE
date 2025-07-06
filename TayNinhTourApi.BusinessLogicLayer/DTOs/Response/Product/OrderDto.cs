5ec7db5bb16b24acc4ae78cd79355586760417d5
Bùi Minh Quang <144604032+LebronBui@users.noreply.github.com>
2025-07-06 21:32:31 +0700

    Update OrderDto.cs

--- TayNinhTourApi.BusinessLogicLayer/DTOs/Response/Product/OrderDto.cs
+++ TayNinhTourApi.BusinessLogicLayer/DTOs/Response/Product/OrderDto.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TayNinhTourApi.DataAccessLayer.Enums;
 
 namespace TayNinhTourApi.BusinessLogicLayer.DTOs.Response.Product
 {
-    internal class OrderDto
+    public class OrderDto
     {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal TotalAfterDiscount { get; set; }
+        public OrderStatus Status { get; set; }
+        public string? VoucherCode { get; set; }
+        public long? PayOsOrderCode { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<OrderDetailDto> OrderDetails { get; set; } = new();
     }
 }
