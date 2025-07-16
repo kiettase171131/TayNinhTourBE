24871e8577b80211c20a29ab7aabe042d0bdfd08
kiettase171131 <kiettase171131@fpt.edu.vn>
2025-07-16 17:00:23 +0700

    dto login

---
+++ TayNinhTourApi.BusinessLogicLayer/DTOs/Request/Authentication/GoogleLoginRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TayNinhTourApi.BusinessLogicLayer.DTOs.Request.Authentication
+{
+    public class GoogleLoginRequestDto
+    {
+        public string IdToken { get; set; }
+    }
+
+}
