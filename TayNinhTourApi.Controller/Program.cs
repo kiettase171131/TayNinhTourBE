5b63c48f0eb980b1796aa1dfdf48380d2d37889c
kiettase171131 <kiettase171131@fpt.edu.vn>
2025-07-21 21:05:05 +0700

    dang ki

--- TayNinhTourApi.Controller/Program.cs
+++ TayNinhTourApi.Controller/Program.cs
@@ -234,6 +234,7 @@ builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<ITourGuideInvitationRepository, TourGuideInvitationRepository>();
 builder.Services.AddScoped<ICartRepository, CartRepository>();
 builder.Services.AddScoped<IVoucherRepository, VoucherRepository>();
+builder.Services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
 
 // Notification Repository
 builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
