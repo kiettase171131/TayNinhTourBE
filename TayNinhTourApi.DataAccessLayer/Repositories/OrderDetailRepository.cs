08d217472b09e3e1a8fc0e62f2f7155714995863
kiettase171131 <kiettase171131@fpt.edu.vn>
2025-07-21 21:05:21 +0700

    create repo

---
+++ TayNinhTourApi.DataAccessLayer/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TayNinhTourApi.DataAccessLayer.Contexts;
+using TayNinhTourApi.DataAccessLayer.Entities;
+using TayNinhTourApi.DataAccessLayer.Repositories.Interface;
+
+namespace TayNinhTourApi.DataAccessLayer.Repositories
+{
+    public class OrderDetailRepository : GenericRepository<OrderDetail>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(TayNinhTouApiDbContext context) : base(context)
+        {
+        }
+    }
+}
