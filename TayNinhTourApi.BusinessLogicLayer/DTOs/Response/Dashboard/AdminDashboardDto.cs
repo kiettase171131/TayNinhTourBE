f5296cbe52c3384212d3aa5b1193610c7f61ccbc
kiettase171131 <kiettase171131@fpt.edu.vn>
2025-08-02 16:25:04 +0700

    dto

---
+++ TayNinhTourApi.BusinessLogicLayer/DTOs/Response/Dashboard/AdminDashboardDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TayNinhTourApi.BusinessLogicLayer.DTOs.Response.Dashboard
+{
+    public class AdminDashboardDto 
+    {
+        public int TotalAccounts { get; set; }
+        public int NewAccountsThisMonth { get; set; }
+        public int BookingsThisMonth { get; set; }
+        public int OrdersThisMonth { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public decimal WithdrawRequestsTotal { get; set; }
+        public int NewTourGuidesThisMonth { get; set; }
+        public int NewShopsThisMonth { get; set; }
+        public int NewPostsThisMonth { get; set; }
+    }
+}
